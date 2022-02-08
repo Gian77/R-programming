c7fa2e3bac0d4ff9fd79f8c4f22fc78bc07aeb6a
Gian77 <gian.benucci@gmail.com>
2022-02-08 14:34:30 -0500

    new tricks

--- String_tricks.R
+++ String_tricks.R
@@ -1,3 +1,13 @@
 # Here some code to play with strings
 
+# Search for patters in R objects
 
+# Searchign for rownames matchigna patterm in a dataframe
+grep(
+  pattern = "Sam*",
+  x = rownames(dataframe),
+  value = TRUE
+)
+
+dataframe %>%
+  dplyr::filter(grepl("Sam*", dataframe$SampleID))
